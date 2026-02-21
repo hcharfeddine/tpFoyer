f7543a5f7ee18ad6065502f0fa05027b2584ad52
haddoukcharfeddine <charfeddine.HADDOUK@esprit.tn>
2026-02-21 22:02:49 +0100

    Update login feature

--- src/test/java/tn/esprit/tpfoyer17/TpFoyer17ApplicationTests.java
+++ src/test/java/tn/esprit/tpfoyer17/TpFoyer17ApplicationTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 class TpFoyer17ApplicationTests {
 
     @Test
